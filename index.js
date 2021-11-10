d74f51f64bf14a68adcef40ce486dd91b719604a
mgalz <78116433+mgalz@users.noreply.github.com>
2021-11-09 22:25:33 -0500

    finishing boxOffice

--- index.js
+++ index.js
@@ -53,7 +53,12 @@ app.get('/Results', function(req, res){
 //Box Office - Chilinski
 app.get('/BoxOffice', function(req, res){
 
-    var imdbdata = 'https://imdb-api.com/en/API/BoxOffice/' + imdb_KEY;
+    var params = 'https://imdb-api.com/en/API/BoxOffice/' + imdb_KEY;
+
+    request(params, async function(err, resp, body){
+        if(!err && resp.statusCode == 200){
+
+    var imdbdata = JSON.parse(body);
         console.log(imdbdata);
 
     var client = await MongoClient.connect(url, {useNewUrlParser: true});
@@ -69,7 +74,9 @@ app.get('/BoxOffice', function(req, res){
     var imdb_mongoData = results[0];
         console.log(imdb_mongoData);
         res.render('BoxOffice', {data: imdb_mongoData});
-        }); 
+            }
+        });
+    });
 
 app.listen(1000, function(){
     console.log('Web App is now running on Port: 1000');
